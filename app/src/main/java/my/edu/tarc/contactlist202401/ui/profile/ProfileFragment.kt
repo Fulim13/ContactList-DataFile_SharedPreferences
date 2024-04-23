de3bb821ea01894ff0eed9a92a0c885f3d5797da
Fu Lim <fulim1130@gmail.com>
2024-04-23 16:06:25 +0800

    DataFile

--- app/src/main/java/my/edu/tarc/contactlist202401/ui/profile/ProfileFragment.kt
+++ app/src/main/java/my/edu/tarc/contactlist202401/ui/profile/ProfileFragment.kt
@@ -2,6 +2,9 @@ package my.edu.tarc.contactlist202401.ui.profile
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -9,6 +12,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
@@ -18,6 +22,10 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import my.edu.tarc.contactlist202401.R
 import my.edu.tarc.contactlist202401.databinding.FragmentProfileBinding
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -30,6 +38,11 @@ class ProfileFragment : Fragment(), MenuProvider {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var sharedPreferences: SharedPreferences
+
+    private val getProfilePicture = registerForActivityResult(
+        ActivityResultContracts.GetContent()){
+        binding.imageViewProfile.setImageURI(it)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,13 +57,18 @@ class ProfileFragment : Fragment(), MenuProvider {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        readPreferences()
+//        readPreferences()
+        readProfilePicture()
 
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         binding.buttonSaveProfile.setOnClickListener {
-            writePreferences()
+            writeProfilePicture()
+        }
+
+        binding.imageViewProfile.setOnClickListener {
+            getProfilePicture.launch("image/*")
         }
     }
 
@@ -95,6 +113,41 @@ class ProfileFragment : Fragment(), MenuProvider {
             putString("email", email)
             putString("phone", phone)
             apply() //writes the updates to the shared preference file
+        }
+    }
+
+    private fun readProfilePicture() {
+        val filename = "jennie-1.png"
+        val file = File(this.context?.filesDir, filename)
+
+        try {
+            if (!file.exists()) {
+                binding.imageViewProfile.setImageResource(R.drawable.profile)
+            } else {
+                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                binding.imageViewProfile.setImageBitmap(bitmap)
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun  writeProfilePicture(){
+        val filename = "jennie-1.png"
+        // this.context?.filesDir = get the app file directory
+        val file = File(this.context?.filesDir, filename)
+
+        val bd = binding.imageViewProfile.drawable as BitmapDrawable
+        val bitmap = bd.bitmap
+        val outputStream: OutputStream
+
+        try{
+            outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        }catch (e: FileNotFoundException){
+            e.printStackTrace()
         }
     }
 }
